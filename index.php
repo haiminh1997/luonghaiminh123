d41eed75de24c9377f5d51595121729f09cb3af0
haiminh1997 <haiminh97lqd@gmail.com>
2019-05-21 19:25:22 +0700

    abc

--- index.php
+++ index.php
@@ -1,8 +1,31 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: HaiMinh
- * Date: 5/21/2019
- * Time: 6:39 PM
- */
-phpinfo();
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Title</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+</head>
+<body>
+<div class="container">
+    <div class="row">
+        <div class="col-sm-12">
+            <h1>Tính chỉ số BMI</h1>
+            <form name="BMI" action="" method="POST">
+                <div class="form-group">
+                    <label >Chiều cao(cm)</label>
+                    <input type="text" class="form-control" placeholder="Height" value="">
+
+                </div>
+                <div class="form-group">
+                    <label >Cân nặng</label>
+                    <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
+                </div>
+
+                <button type="submit" class="btn btn-primary">Submit</button>
+            </form>
+        </div>
+    </div>
+</div>
+
+</body>
+</html>
